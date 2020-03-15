059e96f29bc43c3c43ec435f2a21caf53d3c1af2
Maurice Falls <falls8019oct@gmail.com>
2020-03-15 21:14:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,18 +1,32 @@
 # Write your code here.
 ##output current place in line 
 ##if no one is in line phrase "the line is currently empty"
-def line(array)
-  the_line = []
-  if array.size == 0
+katz_deli = []
+
+def line(katz_deli)
+  line_positions = [];
+  if katz_deli.empty?
     puts "The line is currently empty."
   else
-    counter = 1
-    array.each do |person| 
-      the_line << "The line is currently: #{counter}. #{person} "
-      counter += 1
-      "The line is currently : #{counter}, #{person}"
+    number = 1
+    katz_deli.each do |customer|
+      line_positions << "#{number}. #{customer}"
+      number += 1
     end
-    the_line
-    end
-  
+    puts "The line is currently: #{line_positions.join(" ")}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
 end
