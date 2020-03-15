27cb5b8452331739846524e5f06ada78322b5bd3
Maurice Falls <falls8019oct@gmail.com>
2020-03-15 20:59:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+##output current place in line 
+##if no one is in line phrase "the line is currently empty"
+def line(array)
+  the_line = []
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+    counter = 1
+    array.each do |person| 
+      the_line << "The line is currently: #{counter}. #{person} "
+      counter += 1
+      "The line is currently : #{counter}, #{person}"
+    end
+    the_line
+    end
+  
+end
